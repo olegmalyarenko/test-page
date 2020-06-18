0e1eae432b77b3d4bdf87d35087705d42584523c
olegmalyarenko <oi.malyarenko@gmail.com>
2020-06-18 12:23:59 +0300

    feat: add slides

--- src/js/slider.js
+++ src/js/slider.js
@@ -2,14 +2,16 @@ const buttonOne = document.getElementById('button1');
 const buttonTwo = document.getElementById('button2');
 const buttonThree = document.getElementById('button3');
 const inputs = document.querySelectorAll('.inputs');
-console.log(inputs);
+const slider = document.querySelector('.slider');
+
 
 buttonOne.addEventListener('click', () => {
     inputs.forEach(el => {
         el.checked = false;
     });
     buttonOne.checked = true;
-    
+    slider.style.background = "url('images/a8b78e2c3b86ebbe67f934d9085fdc94.png') no-repeat center";
+    slider.style.backgroundSize = "cover";
 
 
 })
@@ -19,6 +21,8 @@ buttonTwo.addEventListener('click', () => {
         el.checked = false;
     });
     buttonTwo.checked = true;
+    slider.style.background = "url('images/4da1f49923358f8a595c9d796fb9f995.png') no-repeat center";
+    slider.style.backgroundSize = "cover";
    
 
 })
@@ -28,6 +32,6 @@ buttonThree.addEventListener('click', () => {
         el.checked = false;
     });
     buttonThree.checked = true;
-    
-
+    slider.style.background = "url('images/ec063567d630439f5eeee95840f6dcd0.png') no-repeat center";
+    slider.style.backgroundSize = "cover";
 })
